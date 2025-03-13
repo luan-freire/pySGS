5d1bb35dd00cfd1010dea7cec023fdf1443285b1
Luan Freire <luanbjf@hotmail.com>
2025-03-13 18:04:05 -0300

    Adjust to the get_data function of api.py

--- sgs/api.py
+++ sgs/api.py
@@ -16,46 +16,54 @@ def get_data(ts_code: int, begin: str, end: str) -> List:
     """
 
     url = (
-        "http://api.bcb.gov.br/dados/serie/bcdata.sgs.{}"
+        "https://api.bcb.gov.br/dados/serie/bcdata.sgs.{}"
         "/dados?formato=json&dataInicial={}&dataFinal={}"
     )
     request_url = url.format(ts_code, begin, end)
     response = requests.get(request_url)
     return response.json()
-    
-def get_data_with_strict_range(ts_code: int, begin: str, end: str) -> List:
 
+
+def get_data_with_strict_range(ts_code: int, begin: str, end: str) -> List:
     """
     Request time series data from the SGS API considering a strict range of dates.
-        
+
     SGS API default behaviour returns the last stored value when selected date range have no data.
 
     It is possible to catch this behaviour when the first record date precedes the start date.
-    
+
     This function enforces an empty data set when the first record date precedes the start date, avoiding records out of selected range.
-    
+
     :param ts_code: time serie code.
     :param begin: start date (DD/MM/YYYY).
     :param end: end date (DD/MM/YYYY).
-  
+
     :return: Data in json format or an empty list
     :rtype: list
 
     """
     data = get_data(ts_code, begin, end)
-    
+
     first_record_date = to_datetime(data[0]["data"], "pt")
-    period_start_date = to_datetime(begin, 'pt')
-    
+    period_start_date = to_datetime(begin, "pt")
+
     try:
-        is_out_of_range =  first_record_date < period_start_date  #type: ignore
+        is_out_of_range = first_record_date < period_start_date  # type: ignore
         if is_out_of_range:
             raise ValueError
     except TypeError:
-        print("ERROR: Serie " + str(ts_code) + " - Please, use 'DD/MM/YYYY' format for date strings.")
+        print(
+            "ERROR: Serie "
+            + str(ts_code)
+            + " - Please, use 'DD/MM/YYYY' format for date strings."
+        )
         data = []
     except ValueError:
-        print("WARNING: Serie " + str(ts_code) + " - There is no data for the requested period, but there's previous data.")
+        print(
+            "WARNING: Serie "
+            + str(ts_code)
+            + " - There is no data for the requested period, but there's previous data."
+        )
         data = []
-    
+
     return data
